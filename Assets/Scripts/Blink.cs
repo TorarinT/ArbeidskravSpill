3688bf733397e9e22a442654798823c6d97d19b6
Torarin1 <torarintt@gmail.com>
2024-02-06 11:41:10 +0100

    dawd

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Blink : MonoBehaviour
+{
+    public float health;
+    public float speed;
+    public float delay;
+    public float time;
+    void Start()
+    {
+        
+    }
+    public void MovePattern()
+    {
+        
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
