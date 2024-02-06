0817baf0200c37eec8b6e3a8a57da432a1dc5f7d
TorarinT <watcher.torarin@gmail.com>
2024-02-06 19:00:59 +0100

    Fikset Expert Blink

--- Assets/Scripts/Blink/ExpertBlink.cs
+++ Assets/Scripts/Blink/ExpertBlink.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExpertBlink : MonoBehaviour
+public class ExpertBlink : Blink
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
+        moveDelay = 0.1f;
+        moveSpeed = 12f;
+        moveDistance = 5f;
+        health = 250f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IfHealthIs0();
+
     }
 }
